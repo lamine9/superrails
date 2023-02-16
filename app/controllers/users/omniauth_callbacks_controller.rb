bc5b11be4521548fc678f06d7c22a41c3ddda2c2
sibtiger <sallmamadoulamine9@gmail.com>
2023-02-16 15:03:00 +0000

    Added github omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,17 +2,25 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def google_oauth2
+  def handle_auth(kind)
     user = User.from_omniauth(auth)
     if user.present?
       sign_out_all_scopes
-      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+      flash[:success] = t 'devise.omniauth_callbacks.success', kind: kind
       sign_in_and_redirect user, event: :authentication
     else
       flash[:alert] =
-        t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
+        t 'devise.omniauth_callbacks.failure', kind: kind, reason: "#{auth.info.email} is not authorized."
       redirect_to new_user_session_path
     end
+  end
+
+  def google_oauth2
+    handle_auth('Google')
+  end
+
+  def github
+    handle_auth('Github')
   end
 
   protected
